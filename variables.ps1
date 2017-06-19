f23d5d652404c19bb63846fd58f009ffb4a98e8a
Main Informatica <github@main-informatica.com>
2017-06-19 09:40:02 +0200

    Create variables.ps1

---
+++ variables.ps1
@@ -0,0 +1,14 @@
+# Variables de Entorno
+$version="1.5"
+$servername="HV-EJEMPLO"							# Nombre HOST HyperV
+$maquinas = ('MACHINE1','MACHINE2')	# Nombre de Maquinas a Copias
+$nummax = "3"									# Numero de copias NO rotativas, es decir, una vez existan 7 a la 8 borrarÃ¡ la mas antigua
+$unidaddestino="F:"								# Unidad Donde estan las Imagenes a copiar
+$dirdestino="$unidaddestino\"		            # Directorio Completo donde alberga las copias
+$date = Get-Date -Format yyyyMMdd 				# Fecha
+$smtp = "copias.connectate.com" 				# Servidor SMTP para envio de correos
+$from = "copias@copias.connectate.com <copias@copias.connectate.com>"		    # Desde que cuenta 
+$to = "copias@copias.connectate.com <copias@copias.connectate.com>" 		    # A que cuenta
+$pref="DIARIA"	 								# Prefijo de Copia (se puede usar para indicar la frecuencia de la copia)
+$warnspace="120"                                # Nivel de Alarma en espacio Libre de Destino Medido en GB
+

cf0a1b271e93648dcf5b486b6873c088d2665a96
Main Informatica <github@main-informatica.com>
2017-06-19 10:40:28 +0200

    Update copias-1.4.ps1

--- copias-1.4.ps1
+++ copias-1.4.ps1
@@ -33,9 +33,9 @@
   
 
 # Variables de Entorno
-$servername="HV-MAIN"							# Nombre HOST HyperV
-$maquinas = ('Windows 2012 Server R2 64 bit','The Dude')	# Nombre de Maquinas a Copias
-$nummax = "7"									# Numero de copias NO rotativas, es decir, una vez existan 7 a la 8 borrarÃ¡ la mas antigua
+$servername="HV-EJEMPLO"							# Nombre HOST HyperV
+$maquinas = ('MACHINE1','MACHINE2')	# Nombre de Maquinas a Copias
+$nummax = "3"									# Numero de copias NO rotativas, es decir, una vez existan 7 a la 8 borrarÃ¡ la mas antigua
 $unidaddestino="F:"								# Unidad Donde estan las Imagenes a copiar
 $dirdestino="$unidaddestino\"		            # Directorio Completo donde alberga las copias
 $date = Get-Date -Format yyyyMMdd 				# Fecha
@@ -43,8 +43,7 @@ $smtp = "copias.connectate.com" 				# Servidor SMTP para envio de correos
 $from = "copias@copias.connectate.com <copias@copias.connectate.com>"		    # Desde que cuenta 
 $to = "copias@copias.connectate.com <copias@copias.connectate.com>" 		    # A que cuenta
 $pref="DIARIA"	 								# Prefijo de Copia (se puede usar para indicar la frecuencia de la copia)
-$warnspace="400"                                # Nivel de Alarma en espacio Libre de Destino Medido en GB
-
+$warnspace="120"                                # Nivel de Alarma en espacio Libre de Destino Medido en GB
 
 
 ### Funciones      

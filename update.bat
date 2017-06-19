d7f32a207756ee3e3e06e069b1970db97872f664
Main Informatica <github@main-informatica.com>
2017-06-19 10:31:48 +0200

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
 cd c:\COPIAS\
 rmdir TMP /S
-https://github.com/maininformatica/copias.ps1.git
+git clone https://github.com/maininformatica/copias.ps1.git TMP/
 cd TMP
 copy copias.ps1 ..
 echo "Recuerda Editar el Fichero de Variables"
